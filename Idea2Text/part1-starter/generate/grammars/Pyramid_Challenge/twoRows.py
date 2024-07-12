751b2852aa3f2ee406c61a04abd697f7114345fb
davidhhhhhhh <57957067+davidhhhhhhh@users.noreply.github.com>
2024-07-11 19:38:37 -0800

    save instead of draw

--- Idea2Text/part1-starter/generate/grammars/Pyramid_Challenge/twoRows.py
+++ Idea2Text/part1-starter/generate/grammars/Pyramid_Challenge/twoRows.py
@@ -4,13 +4,24 @@ class TwoRows(Decision):
     def registerChoices(self):
         self.addChoice('codeStructure', {
             '''import acm.graphics.*;
-import acm.program.*;
 import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import javax.imageio.ImageIO;
 
-public class DrawTwoRows extends GraphicsProgram {{
-    public void run() {{
-        // Set canvas size
-        {SetCanvasSizeTwoRows}
+public class DrawStructure{{
+    private static final int CANVAS_WIDTH = 600;
+    private static final int CANVAS_HEIGHT = 400;
+    private static final int IMAGE_WIDTH = 800;
+    private static final int IMAGE_HEIGHT = 600;
+
+    public static void main(String[] args) {{
+        // Create an off-screen GCanvas
+        GCanvas canvas = new GCanvas();
+        canvas.setSize(CANVAS_WIDTH, CANVAS_HEIGHT);
 
         // Initialize brick parameters for first row
         {InitializeBrickParametersTwoRowsFirstRow}
@@ -29,7 +40,7 @@ public class DrawTwoRows extends GraphicsProgram {{
                 brick.setColor({BrickColorTwoRowsFirstRow});
             }}
 
-            add(brick);
+            canvas.add(brick);
         }}
 
         // Initialize brick parameters for second row
@@ -51,12 +62,43 @@ public class DrawTwoRows extends GraphicsProgram {{
 
             add(brick);
         }}
+        // Save the canvas as an image
+        saveCanvasAsImage(canvas);
     }}
+    private static void saveCanvasAsImage(GCanvas canvas) {{
+        BufferedImage image = new BufferedImage(IMAGE_WIDTH, IMAGE_HEIGHT, BufferedImage.TYPE_INT_RGB);
+        Graphics g = image.getGraphics();
+        g.setColor(Color.WHITE);
+        g.fillRect(0, 0, IMAGE_WIDTH, IMAGE_HEIGHT);
 
-    public static void main(String[] args) {{
-        // Start the GraphicsProgram
-        new DrawTwoRows().start(args);
-    }}
+        // Draw the current canvas content to the buffered image
+        BufferedImage canvasImage = new BufferedImage(CANVAS_WIDTH, CANVAS_HEIGHT, BufferedImage.TYPE_INT_RGB);
+        Graphics canvasGraphics = canvasImage.getGraphics();
+        canvasGraphics.setColor(Color.WHITE);
+        canvasGraphics.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+        canvas.paint(canvasGraphics);
+
+        // Center the canvas on the image
+        int offsetX = (IMAGE_WIDTH - CANVAS_WIDTH) / 2;
+        int offsetY = (IMAGE_HEIGHT - CANVAS_HEIGHT) / 2;
+        g.drawImage(canvasImage, offsetX, offsetY, null);
+
+        // Draw a border around the canvas area
+        g.setColor(Color.BLACK);
+        g.drawRect(offsetX, offsetY, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+        // Generate a unique filename using a timestamp
+        String timestamp = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
+        String filename = "twoRows_" + timestamp + ".png";
+
+        try {{
+            // Write the buffered image to a file
+            ImageIO.write(image, "png", new File(filename));
+            System.out.println("Image saved as " + filename);
+        }} catch (Exception e) {{
+            e.printStackTrace();
+        }}
+    }}            
 }}''': 1
         })
 
